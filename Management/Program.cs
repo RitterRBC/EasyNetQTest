4d7f0f47c927a7b4d03667f8f8df75141907ec8b
RitterRBC <thorsten.daempfert@gmail.com>
2023-04-04 15:37:26 +0200

    2023-04-04 Update subs Management clean up exchanges und queues

--- Management/Program.cs
+++ Management/Program.cs
@@ -13,7 +13,7 @@ namespace Management
             {
                 var exchanges = initial.GetExchanges();
 
-                foreach (Exchange exchange in exchanges.Where(x=> (x.Name.StartsWith("OM")||x.Name.StartsWith("IS")) && !x.Name.Contains("-")))
+                foreach (Exchange exchange in exchanges.Where(x=> (x.Name.StartsWith("OM")||x.Name.StartsWith("IS")||x.Name.StartsWith("PrestigeEnterprise")) && !x.Name.Contains("-")))
                 {
                     Console.Out.WriteLine("Delete exchange = {0} ...", exchange.Name);
                     initial.DeleteExchange(exchange);
@@ -21,7 +21,7 @@ namespace Management
 
                 var queues = initial.GetQueues();
 
-                foreach (Queue queue in queues.Where(x=> (x.Name.StartsWith("OM")||x.Name.StartsWith("IS")) && !x.Name.Contains("-")))
+                foreach (Queue queue in queues.Where(x=> (x.Name.StartsWith("OM")||x.Name.StartsWith("IS")||x.Name.StartsWith("PrestigeEnterprise")) && !x.Name.Contains("-")))
                 {
                     Console.Out.WriteLine("Delete queue = {0} ...", queue.Name);
                     initial.DeleteQueue(queue);
