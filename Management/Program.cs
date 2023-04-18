adf9a8c3e70d677b058ccfca66c62023df6c015c
RitterRBC <thorsten.daempfert@gmail.com>
2023-04-18 08:06:50 +0200

    Update subs

--- Management/Program.cs
+++ Management/Program.cs
@@ -13,19 +13,29 @@ namespace Management
             {
                 var exchanges = initial.GetExchanges();
 
-                foreach (Exchange exchange in exchanges.Where(x=> (x.Name.StartsWith("OM")||x.Name.StartsWith("IS")||x.Name.StartsWith("PrestigeEnterprise")) && !x.Name.Contains("-")))
+                foreach (Exchange exchange in exchanges.Where(x => (x.Name.StartsWith("NS") || x.Name.StartsWith("OM") || x.Name.StartsWith("IS") || x.Name.StartsWith("PrestigeEnterprise")) && !x.Name.Contains("-")))
                 {
                     Console.Out.WriteLine("Delete exchange = {0} ...", exchange.Name);
                     initial.DeleteExchange(exchange);
                 }
+                //foreach (Exchange exchange in exchanges.Where(x => (x.Name.StartsWith("NS") || x.Name.StartsWith("OM") || x.Name.StartsWith("IS") || x.Name.StartsWith("PrestigeEnterprise"))))
+                //{
+                //    Console.Out.WriteLine("Delete exchange = {0} ...", exchange.Name);
+                //    initial.DeleteExchange(exchange);
+                //}
 
                 var queues = initial.GetQueues();
 
-                foreach (Queue queue in queues.Where(x=> (x.Name.StartsWith("OM")||x.Name.StartsWith("IS")||x.Name.StartsWith("PrestigeEnterprise")) && !x.Name.Contains("-")))
+                foreach (Queue queue in queues.Where(x => (x.Name.StartsWith("NS") || x.Name.StartsWith("OM") || x.Name.StartsWith("IS") || x.Name.StartsWith("PrestigeEnterprise")) && !x.Name.Contains("-")))
                 {
                     Console.Out.WriteLine("Delete queue = {0} ...", queue.Name);
                     initial.DeleteQueue(queue);
                 }
+                //foreach (Queue queue in queues.Where(x => (x.Name.StartsWith("NS") || x.Name.StartsWith("OM") || x.Name.StartsWith("IS") || x.Name.StartsWith("PrestigeEnterprise"))))
+                //{
+                //    Console.Out.WriteLine("Delete queue = {0} ...", queue.Name);
+                //    initial.DeleteQueue(queue);
+                //}
                 Console.WriteLine("Cleaning transient exchanges and queues. Hit <return> to quit.");
                 Console.ReadLine();
             }
